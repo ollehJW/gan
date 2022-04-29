bf863e8351a2dfb0a8e7f984ad2b19ff9139f610
jongwook95.lee <jongwook95.lee@lge.com>
2022-04-29 16:10:23 +0900

    first commit

---
+++ data_gen.py
@@ -0,0 +1,68 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import cv2
+
+transform_dict = {
+    'train': transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize(128),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, ), (0.5, ))
+    ]),
+    'test': transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize(128),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, ), (0.5, ))
+    ])
+}
+
+class ConstructDataset(Dataset):
+    """
+    Construct pytorch Dataset from file list.
+    Parameters
+    ----------
+    file_list : list
+        image file list
+    phase : str
+        train phase. (Default: 'train')
+    Returns
+    --------
+    pytorch Dataset
+    """
+
+    def __init__(self, file_list, phase = 'train'):
+        self.file_list = file_list
+        self.phase = phase
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, index):
+        file_name = self.file_list[index]
+        image = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
+        image = transform_dict[self.phase](image)
+        return image, 1
+
+class dataset_generator(object):
+    """
+    Construct pytorch DataLoader from file list.
+    Parameters
+    ----------
+    file_list : list
+        image file list
+    batch_size : int
+        batch size. (Default: 16) 
+    Returns
+    --------
+    pytorch DataLoader
+    """
+    def __init__(self, file_list, batch_size = 16):
+        self.file_list = file_list
+        self.batch_size = batch_size
+
+    def dataloader(self):
+        train_dataset = ConstructDataset(self.file_list, phase = 'train')
+        return dict({'train': DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)})
+        
